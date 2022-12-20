bf73d3a9f74a87fcc1fd8f9236db0a2ecc4c4a54
XXX <XXX@XXX.com>
2022-12-20 19:26:32 +0300

    add

--- LABA7/версия 2/script.js
+++ LABA7/версия 2/script.js
@@ -1,11 +1,14 @@
+
 ball.onmousedown = function(event) {
 
     let shiftX = event.clientX - ball.getBoundingClientRect().left;
     let shiftY = event.clientY - ball.getBoundingClientRect().top;
   
     ball.style.position = 'absolute';
-    //ball.style.zIndex = 100;
+    ball.style.zIndex = 1000;
     document.body.append(ball);
+    glLeft = event.pageX;
+    glTop = event.pageY;
   
     moveAt(event.pageX, event.pageY);
   
@@ -17,7 +20,9 @@ ball.onmousedown = function(event) {
     }
 
     function onMouseMove(event) {
+      if(ball.getBoundingClientRect().height-70<event.clientY){
         moveAt(event.pageX, event.pageY);
+      }
       }
     
       // передвигаем мяч при событии mousemove
@@ -26,11 +31,53 @@ ball.onmousedown = function(event) {
       // отпустить мяч, удалить ненужные обработчики
       ball.onmouseup = function() {
         document.removeEventListener('mousemove', onMouseMove);
-        ball.onmouseup = null;
-      };
+        let tempx = ball.style.left.split("px")[0];
+        let temoy = ball.style.top.split("px")[0];
+        moveAt((glLeft - (tempx - glLeft)), (glTop - (temoy - glTop)))
+        myMove(tempx,temoy,(glLeft - (tempx - glLeft)),(glTop - (temoy - glTop)),ball)
+        setTimeout(() => {
+
+        moveAt(glLeft, glTop)
+      },4000);
+
+      ball.onmouseup = null;
+};
+
     
     };
     
     ball.ondragstart = function() {
       return false;
     };
+
+    function myMove(posLSTART, posTSTART,posLEND, posTEND,ball) {
+      let id = setInterval(frame, 5);
+      console.log(posLSTART)
+      console.log(posLEND)
+      function frame() {
+      console.log(posTSTART)
+      console.log(posTEND)
+      if (posTSTART<=posTEND) {
+        clearInterval(id);
+} else {
+if(posTSTART!=posTEND){
+
+if((posTSTART-200)>posTEND){
+posTSTART-=2;
+}else{
+posTSTART-=4;
+}
+
+}
+//!! continue increment
+if(posLSTART>posLEND) {
+posLSTART--;
+}else {
+posLSTART++;
+}
+
+ball.style.top = posTSTART + 'px';
+ball.style.left = posLSTART + 'px';
+}
+}
+}
